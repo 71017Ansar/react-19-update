20dc04e99ae05d611a02c6a606e33c976fda828d
71017Ansar <71017@gcslahore.edu.pk>
2025-02-18 13:41:24 +0500

    react 19 undersanding new feactures

---
+++ src/app/components/React19.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function React19() {
+    const name = 'John';
+    const age = 30;
+    function sayHello() {
+         let name = 'Jane';
+        let   age= 30;
+        return <h1>Hello {name}and age is {age}</h1>
+    }
+  return (
+    <div>
+        <h1>React 19</h1>
+        <p>My name is {name}</p>
+        <h2>{sayHello()}</h2>
+        <span>{ age<18 ? "ansar" : "Guest"}</span>
+      
+    </div>
+  )
+}
