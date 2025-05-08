a915fc1169f7c64000005b74a68d47f6910c3a80
71017Ansar <71017@gcslahore.edu.pk>
2025-05-08 16:16:38 +0500

    shadcn ui pie chart

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,23 @@
-import Image from "next/image";
-import Gcsform from "./components/Gcsform";
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-// import React19 from "./components/React19";
+import AppAeraChart from "@/components/AppAeraChart";
+import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
+
 
 
 export default function Home() {
 
   return (
-    <div className="">
-     
+    <div className="grid grid-cols-1  lg:grid-cols-2 2xl-:grid-cols-4 gap-4 p-4">
 
-      
-      
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppAeraChart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Card 3</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Card 4</div>
+      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppPieChart/></div>
+      <div className="bg-primary-foreground p-4 rounded-lg">Card 6</div>
+
     </div>
   );
 }
--- src/components/ui/AppSiderbar.jsx
+++ src/components/ui/AppSiderbar.jsx
@@ -1,8 +1,9 @@
-import { Home, Inbox, Calendar, Search, Settings, User2, User, ChevronUp } from 'lucide-react'
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from './sidebar'
+import { Home, Inbox, Calendar, Search, Settings, User2, User, ChevronUp, Plus, Projector, ChevronDown, PlusIcon } from 'lucide-react'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupAction, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuBadge, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from './sidebar'
 import Link from 'next/link'
 import Image from 'next/image'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@radix-ui/react-collapsible'
 
 // Create an icon map
 const iconMap = {
@@ -69,16 +70,93 @@ export default function AppSidebar() {
               const Icon = iconMap[item.icon]
               return (
                 <SidebarMenuItem key={item.title}>
+                  {/* <SidebarMenuButton asChild> */}
                   <Link href={item.url} passHref legacyBehavior>
                     <a className="flex items-center gap-2 px-4 py-2 text-sm font-medium rounded-md hover:bg-accent hover:text-accent-foreground">
                       <Icon className="w-4 h-4" />
                       <span>{item.title}</span>
                     </a>
                   </Link>
+                  {/* </SidebarMenuButton> */}
+                  {item.title==='Inbox' && (
+                    <SidebarMenuBadge>24</SidebarMenuBadge>
+                  ) }
                 </SidebarMenuItem>
               )
             })}
           </SidebarMenu>
+        </SidebarGroup>
+        <Collapsible defaultOpen className="group/collapsible">
+        <SidebarGroup>
+          <SidebarGroupLabel asChild>
+            <CollapsibleTrigger>
+            Collapsabile
+            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+          </CollapsibleTrigger></SidebarGroupLabel>
+          {/* <SidebarGroupAction>
+            <Plus /> <span className="sr-only">Add Project</span>
+          </SidebarGroupAction> */}
+          <CollapsibleContent>
+          <SidebarContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/#" >
+                  <Projector/>
+                  <span>See all projects</span>
+                  </Link>
+
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/#" >
+                  <Plus/>
+                  <span>Add Project</span>
+                  </Link>
+
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarContent>
+          </CollapsibleContent>
+        </SidebarGroup>
+        </Collapsible>
+        <SidebarGroup>
+          <SidebarGroupLabel>Nested items</SidebarGroupLabel>
+          <SidebarContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link href="/#" >
+                  <Projector/>
+                      Add Projects
+                  </Link>
+
+                </SidebarMenuButton>
+                <SidebarMenuSub>
+                  <SidebarMenuSubItem>
+                    <SidebarMenuSubButton asChild>
+                      <Link href="/#" >
+                      <Plus/>
+                      Add Projects
+                      </Link>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                  <SidebarMenuSubItem>
+                    <SidebarMenuSubButton asChild>
+                      <Link href="/#" >
+                      <Plus/>
+                      Add category
+                      </Link>
+                    </SidebarMenuSubButton>
+                  </SidebarMenuSubItem>
+                </SidebarMenuSub>
+              </SidebarMenuItem>
+             
+            </SidebarMenu>
+
+          </SidebarContent>
         </SidebarGroup>
       </SidebarContent>
       
