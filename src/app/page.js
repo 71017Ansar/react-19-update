f93539dc4c0d0bfe68d3f674036d5a427f0fef81
71017Ansar <71017@gcslahore.edu.pk>
2025-05-12 11:04:35 +0500

    shadcn ui
    mastary project

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,30 @@
 import AppAeraChart from "@/components/AppAeraChart";
 import AppBarChart from "@/components/AppBarChart";
 import AppPieChart from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 
 
 export default function Home() {
 
   return (
-    <div className="grid grid-cols-1  lg:grid-cols-2 2xl-:grid-cols-4 gap-4 p-4">
-
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        <AppBarChart />
-      </div>
-      <div className="bg-primary-foreground p-4 rounded-lg"><AppAeraChart/></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Card 3</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Card 4</div>
-      <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppPieChart/></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Card 6</div>
-
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppBarChart />
     </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">
+      <CardList title="Latest Transactions" />
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">
+      <AppPieChart />
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg"></div>
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppAeraChart />
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">
+      <CardList title="Popular Content" />
+    </div>
+  </div>
   );
 }
